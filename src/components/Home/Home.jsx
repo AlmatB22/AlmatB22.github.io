daeb808e1a4b26c95d3ebc9825e29ebada23786c
almatbolatbekov <almat.bolatbek22@gmail.com>
2025-04-30 15:01:45 -0700

    Added description on the home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,7 +8,11 @@ function Home ({changePage}) {
     return (
         <div id='home'>
             <div id='home-img'><img src='https://media.discordapp.net/attachments/1327069432500981771/1329860896700366948/picture.jpg?ex=68110879&is=680fb6f9&hm=26a8ee53a4add713cefa5017af30a14e30ecd36202f3df7bb4cdb4ccd343e05c&=&format=webp&width=1310&height=1310' /></div>
-            <div id='home-greeting'>Description</div>
+            <div id='home-greeting'>
+                <h1>Hi, I'm Almat!</h1>
+                <p>I'm a Software Engineering graduate passionate about building impactful solutions in software development, machine learning, and data analytics.</p>
+            </div>
+
             <div className='home-card' onClick={() => handleClick('about')}>About Me</div>
             <div className='home-card' onClick={() => handleClick('experience')}>Work Experience</div>
             <div className='home-card' onClick={() => handleClick('project')}>Projects</div>
